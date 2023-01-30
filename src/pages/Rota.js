9d62269b62b5e18f78e723ad7c5e515f508e1473
Guilherme Zanin <guizanin224@gmail.com>
2023-01-30 11:39:05 +0000

    fixed svg tags

--- src/pages/Rota.js
+++ src/pages/Rota.js
@@ -29,13 +29,13 @@ const Rota = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.0"
+                  strokeWidth="1.0"
                   stroke="currentColor"
                   width="45px"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
                   />
                 </svg>
@@ -48,13 +48,13 @@ const Rota = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   width="45px"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
                   />
                 </svg>
@@ -65,7 +65,7 @@ const Rota = () => {
           </div>
           <NamesModal />
           <DatesModal />
-          <div class="grid grid-cols-2 divide-x-2">
+          <div className="grid grid-cols-2 divide-x-2">
             <div className="pr-[20px]">
               <h1 className='text-[30px]	font-semibold'>Nomes cadastrados</h1>
               <ul>
